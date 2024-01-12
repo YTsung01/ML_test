c84354adb62162bc55e0e84e54fc9f8cd3523ee7
YTsung <songyuting@songyutingdeiMac.local>
2024-01-12 14:53:28 +0800

    TEST

--- ML_test.py
+++ ML_test.py
@@ -12,47 +12,46 @@ import pandas as pd
 from scipy.stats import kurtosis
 from scipy.stats import skew
 
-data=pd.read_hdf("FallAllD.h5")  #可以看到資料
-newdata=data.copy()
-nd=(newdata[newdata["Device"].isin(["Waist"])]).reset_index() #nd為只有腰部的資料
+data=pd.read_hdf("FallAllD.h5")  
+waist_data=(data[data["Device"].isin(["Waist"])]).reset_index() #waist_data為只有腰部的資料
   
-k1=(nd['ActivityID']).values.reshape(-1, 1)
+TargetVector=(waist_data['ActivityID']).values.reshape(-1, 1)
 for i in range(0,1798):
-    if k1[i]>=100:
-        k1[i]=1
+    if TargetVector[i]>=100:
+        TargetVector[i]=1
         
     else:
-        k1[i]=0         #將大於100的變成1 小於100的變成0        
-TargetVector=k1   
+        TargetVector[i]=0         #將大於100的變成1 小於100的變成0        
+
 
 #%%
 waist=[]
-for i in range(len(nd)):
-    test01=nd['Acc'][i]
-    waist.append(test01)         #將3軸ACC的資料取出 存在waist裡面
+for i in range(len(waist_data)):
+    acc=waist_data['Acc'][i]
+    waist.append(acc)         #將3軸ACC的資料取出 存在waist裡面
     
      
 norm=[]
 for i in range(len(waist)):
-    a0=np.array(waist[i][:,0], dtype='int64')**2
-    a1=np.array(waist[i][:,1], dtype='int64')**2
-    a2=np.array(waist[i][:,2], dtype='int64')**2
-    test02=np.sqrt(a0+a1+a2)
-    norm.append(test02)
+    x=np.array(waist[i][:,0], dtype='int64')**2 #取xyz軸的平方
+    y=np.array(waist[i][:,1], dtype='int64')**2
+    z=np.array(waist[i][:,2], dtype='int64')**2
+    square=np.sqrt(x+y+z)   #xyz軸的平方 開根號
+    norm.append(square)
     
 hori=[]
 for i in range(len(waist)):
-    a1=np.array(waist[i][:,1], dtype='int64')**2
-    a2=np.array(waist[i][:,2], dtype='int64')**2
-    test02_1=np.sqrt(a1+a2)
-    hori.append(test02_1) 
+    y=np.array(waist[i][:,1], dtype='int64')**2
+    z=np.array(waist[i][:,2], dtype='int64')**2
+    square=np.sqrt(y+z)
+    hori.append(square) 
     
 verti=[]
 for i in range(len(waist)):
-    a0=np.array(waist[i][:,0], dtype='int64')**2
-    a1=np.array(waist[i][:,1], dtype='int64')**2
-    test02_2=np.sqrt(a0+a1)
-    verti.append(test02_2)
+    x=np.array(waist[i][:,0], dtype='int64')**2
+    y=np.array(waist[i][:,1], dtype='int64')**2
+    square=np.sqrt(x+y)
+    verti.append(square)
     
     
 waist=np.array(waist)       
@@ -60,13 +59,13 @@ norm=np.array(norm)
 hori=np.array(hori)
 verti=np.array(verti)
 
-d=np.reshape(norm,(1798,4760,1))
-waist=np.concatenate((waist,d),axis=2)   #將waist3軸ACC的資料和norm合併成一個矩陣
+data_reshape=np.reshape(norm,(1798,4760,1))
+waist=np.concatenate((waist,data_reshape),axis=2)   #將waist3軸ACC的資料和norm合併成一個矩陣
 
-d=np.reshape(hori,(1798,4760,1))
-waist=np.concatenate((waist,d),axis=2)
-d=np.reshape(verti,(1798,4760,1))
-waist=np.concatenate((waist,d),axis=2)
+data_reshape=np.reshape(hori,(1798,4760,1))
+waist=np.concatenate((waist,data_reshape),axis=2)
+data_reshape=np.reshape(verti,(1798,4760,1))
+waist=np.concatenate((waist,data_reshape),axis=2)
 
 #%%  取feacture
 featuremean=[]
@@ -80,22 +79,22 @@ featurerange=[]
 
 for j in range(6):
     for i in range(len(waist)):
-        type01=np.mean(waist[i,:,j]) 
-        featuremean.append(type01)
-        type02=np.std(waist[i,:,j])
-        featurestd.append(type02)
-        type03=np.var(waist[i,:,j])
-        featurevar.append(type03)
-        type04=np.max(waist[i,:,j])
-        featuremax.append(type04)
-        type05=np.min(waist[i,:,j])
-        featuremin.append(type05)
-        type06=kurtosis(waist[i,:,j])
-        featurekurtosis.append(type06)
-        type07=skew(waist[i,:,j])
-        featureskew.append(type07)
-        type08=type04-type05
-        featurerange.append(type08)
+        data_mean=np.mean(waist[i,:,j]) 
+        featuremean.append(data_mean)
+        data_std=np.std(waist[i,:,j])
+        featurestd.append(data_std)
+        data_var=np.var(waist[i,:,j])
+        featurevar.append(data_var)
+        data_max=np.max(waist[i,:,j])
+        featuremax.append(data_max)
+        data_min=np.min(waist[i,:,j])
+        featuremin.append(data_min)
+        data_kurtosis=kurtosis(waist[i,:,j])
+        featurekurtosis.append(data_kurtosis)
+        data_skew=skew(waist[i,:,j])
+        featureskew.append(data_skew)
+        data_range=data_max-data_min
+        featurerange.append(data_range)
         
               
 featuremean=np.array(featuremean)        
@@ -116,31 +115,31 @@ featurerange=np.array(featurerange)
 featurerange=np.reshape(featurerange,(6,1798))   
 
 #%% 取feacture_Correlation coefficien
-xycc=[]
-xzcc=[]
-yzcc=[]
-nvcc=[]
-nhcc=[]
-vhcc=[]
+xycc=[]  #x軸跟y軸的Correlation coefficien
+xzcc=[]  #x軸跟z軸的Correlation coefficien
+yzcc=[]  #y軸跟z軸的Correlation coefficien
+nvcc=[]  #norm軸跟verti軸的Correlation coefficien
+nhcc=[]  #norm軸跟hori軸的Correlation coefficien
+vhcc=[]  #verti軸跟hori軸的Correlation coefficien
 for i in range(len(waist)):
-    pccs01 = np.corrcoef(waist[i,:,0], waist[i,:,1]) #x軸跟y軸的Correlation coefficien
-    pccs01=pccs01[0,1]
-    xycc.append(pccs01)
-    pccs02 = np.corrcoef(waist[i,:,0], waist[i,:,2]) #x軸跟z軸的Correlation coefficien
-    pccs02=pccs02[0,1]
-    xzcc.append(pccs02)
-    pccs03 = np.corrcoef(waist[i,:,1], waist[i,:,2]) #y軸跟z軸的Correlation coefficien
-    pccs03=pccs03[0,1]
-    yzcc.append(pccs03)
-    pccs04 = np.corrcoef(waist[i,:,3], waist[i,:,5]) #norm軸跟verti軸的Correlation coefficien
-    pccs04=pccs04[0,1]
-    nvcc.append(pccs04)
-    pccs05 = np.corrcoef(waist[i,:,3], waist[i,:,4]) #norm軸跟hori軸的Correlation coefficien
-    pccs05=pccs05[0,1]
-    nhcc.append(pccs05)
-    pccs06 = np.corrcoef(waist[i,:,5], waist[i,:,4]) #verti軸跟hori軸的Correlation coefficien
-    pccs06=pccs06[0,1]
-    vhcc.append(pccs06)
+    pccs = np.corrcoef(waist[i,:,0], waist[i,:,1]) #x軸跟y軸的Correlation coefficien
+    pccs=pccs[0,1]
+    xycc.append(pccs)
+    pccs = np.corrcoef(waist[i,:,0], waist[i,:,2]) #x軸跟z軸的Correlation coefficien
+    pccs=pccs[0,1]
+    xzcc.append(pccs)
+    pccs = np.corrcoef(waist[i,:,1], waist[i,:,2]) #y軸跟z軸的Correlation coefficien
+    pccs=pccs[0,1]
+    yzcc.append(pccs)
+    pccs = np.corrcoef(waist[i,:,3], waist[i,:,5]) #norm軸跟verti軸的Correlation coefficien
+    pccs=pccs[0,1]
+    nvcc.append(pccs)
+    pccs = np.corrcoef(waist[i,:,3], waist[i,:,4]) #norm軸跟hori軸的Correlation coefficien
+    pccs=pccs[0,1]
+    nhcc.append(pccs)
+    pccs = np.corrcoef(waist[i,:,5], waist[i,:,4]) #verti軸跟hori軸的Correlation coefficien
+    pccs=pccs[0,1]
+    vhcc.append(pccs)
 
 xycc=np.array(xycc) 
 xycc=np.reshape(xycc,(1,1798))
@@ -162,12 +161,10 @@ feature=np.concatenate((featuremean,featurestd,featurevar,featuremax,\
                              
     
 #%%    
-rr=np.rot90(feature,k=1,axes=(0,1))
-rrr=np.flipud(TargetVector)
+X=np.rot90(feature,k=1,axes=(0,1))
+Y=np.flipud(TargetVector)
 from sklearn import metrics
 from sklearn.svm import SVC
-X=rr
-Y=rrr
 model = SVC()
 model.fit(X,Y)
 expected = Y
@@ -175,7 +172,8 @@ predicted = model.predict(X)
 print(metrics.classification_report(expected ,predicted))
 print(metrics.confusion_matrix(expected ,predicted))
 
-#%%
+#%%LeaveOneOut
+ 
 from sklearn.model_selection import LeaveOneOut
 loo = LeaveOneOut()
 loo.get_n_splits(X)
@@ -194,4 +192,3 @@ for train_index, test_index in loo.split(Y):
       
 print(metrics.classification_report(Y,output))
 print(metrics.confusion_matrix(Y,output))    
-
